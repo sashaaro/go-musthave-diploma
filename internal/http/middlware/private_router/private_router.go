41ceafaf393e6be9c8633151926ed1b7ae8d3bf1
Aleksandr Arofikin <sashaaro@gmail.com>
2024-07-16 18:46:13 +0300

    add intergation

--- internal/http/middlware/private_router/private_router.go
+++ internal/http/middlware/private_router/private_router.go
@@ -2,8 +2,8 @@ package privateRouter
 
 import (
 	"context"
-	"github.com/GTech1256/go-musthave-diploma-tpl/internal/config"
-	logging2 "github.com/GTech1256/go-musthave-diploma-tpl/pkg/logging"
+	"github.com/sashaaro/go-musthave-diploma-tpl/internal/config"
+	logging2 "github.com/sashaaro/go-musthave-diploma-tpl/pkg/logging"
 	"net/http"
 )
 
